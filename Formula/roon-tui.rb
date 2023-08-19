52070775c1dd3a481cc93f162fde589b08ea4dfa
Bart Verhoeven <bartverhoeven@live.com>
2023-08-19 21:29:29 +0800

    Set explicit version number.

--- Formula/roon-tui.rb
+++ Formula/roon-tui.rb
@@ -1,9 +1,12 @@
 class RoonTui < Formula
+  version "0.0.2"
+  license "MIT"
+
   desc "A Roon Remote for the terminal"
   homepage "https://github.com/TheAppgineer/roon-tui"
-  url "https://github.com/TheAppgineer/roon-tui/archive/0.0.2.tar.gz"
+
+  url "https://github.com/TheAppgineer/roon-tui/archive/#{version}.tar.gz"
   sha256 "8ffbc2db0a72165ab1ed83736784ec0f172a8fa36866fe616f4cc27bc220c895"
-  license "MIT"
 
   depends_on "rust" => :build
 
