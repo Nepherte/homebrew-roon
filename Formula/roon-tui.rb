7eb753a8d3590d2ecf7105b31233e968a0d76c98
Bart Verhoeven <bartverhoeven@live.com>
2023-08-22 12:25:51 +0800

    Version can be automatically derived from the url (brew audit).

--- Formula/roon-tui.rb
+++ Formula/roon-tui.rb
@@ -1,11 +1,10 @@
 class RoonTui < Formula
-  version "0.0.3"
   license "MIT"
 
   desc "Roon Remote for the terminal"
   homepage "https://github.com/TheAppgineer/roon-tui"
 
-  url "https://github.com/TheAppgineer/roon-tui.git", tag: "#{version}"
+  url "https://github.com/TheAppgineer/roon-tui.git", tag: "0.0.3"
   head "https://github.com/TheAppgineer/roon-tui.git", branch: "develop"
   sha256 "9491bf36af59c4498e997a09a7fba745ce734f16d508ae30dd687986c8cbed88"
 
