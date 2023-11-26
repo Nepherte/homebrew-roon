082f6522cd24d7c6d2901672f219849ae5294b70
Bart Verhoeven <bartverhoeven@live.com>
2023-11-26 10:03:41 +0800

    Inline version number for compatibility with bump pr process.

--- Formula/roon-tui.rb
+++ Formula/roon-tui.rb
@@ -1,13 +1,11 @@
 class RoonTui < Formula
-  version "0.1.0"
-  sha256 "ab687b3ea38972569153d4757a417cab840ca34ce9ecf06aa43e06c61e485be0"
-
-  url "https://github.com/TheAppgineer/roon-tui/archive/refs/tags/#{version}.tar.gz"
-  head "https://github.com/TheAppgineer/roon-tui.git", branch: "master"
   desc "Roon Remote for the terminal"
   homepage "https://github.com/TheAppgineer/roon-tui"
-
+  url "https://github.com/TheAppgineer/roon-tui/archive/refs/tags/1.0.0.tar.gz"
+  head "https://github.com/TheAppgineer/roon-tui.git", branch: "master"
+  sha256 "ab687b3ea38972569153d4757a417cab840ca34ce9ecf06aa43e06c61e485be0"
   license "MIT"
+
   depends_on "rust" => :build
 
   def install
