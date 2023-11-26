fcd6192ffcbacb05c797c14f7dd7636bc4befbb7
Bart Verhoeven <bartverhoeven@live.com>
2023-11-26 09:10:34 +0800

    Add dummy checksum hoping to fix bump pr process.

--- Formula/roon-tui.rb
+++ Formula/roon-tui.rb
@@ -3,6 +3,7 @@ class RoonTui < Formula
   homepage "https://github.com/TheAppgineer/roon-tui"
 
   url "https://github.com/TheAppgineer/roon-tui.git", tag: "0.1.0"
+  sha256 "1b5bdb6be9f9601b6951672f99a18cb89c0d6e59ccc1453314f94b4d0579271b"
   head "https://github.com/TheAppgineer/roon-tui.git", branch: "master"
 
   license "MIT"
