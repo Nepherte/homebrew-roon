e790e17f59c14773d977d10941de2c960e7d3d06
Bart Verhoeven <bartverhoeven@pm.me>
2024-05-15 08:35:42 +0800

    roon-tui 0.3.1 (#3)

--- Formula/roon-tui.rb
+++ Formula/roon-tui.rb
@@ -1,9 +1,9 @@
 class RoonTui < Formula
   desc "Roon Remote for the terminal"
   homepage "https://github.com/TheAppgineer/roon-tui"
-  url "https://github.com/TheAppgineer/roon-tui/archive/refs/tags/0.3.0.tar.gz"
+  url "https://github.com/TheAppgineer/roon-tui/archive/refs/tags/0.3.1.tar.gz"
   head "https://github.com/TheAppgineer/roon-tui.git", branch: "master"
-  sha256 "4187510ecc2e1b34636c41641cacf1090e21c00819829e6c0539d24e2e85ffc6"
+  sha256 "5f29a3fcd9e0b136e4b606e6287b0705d58ffb7ef277d88e8c808d2b1c3a035d"
   license "MIT"
 
   depends_on "rust" => :build
